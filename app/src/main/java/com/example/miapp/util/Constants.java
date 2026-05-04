1a1f83fd6bfd7eb477712c8cf48855d0a7cbe866
fede-egas <egasagustin@gmail.com>
2026-05-04 20:32:52 -0300

    Clase urls peliculas

---
+++ app/src/main/java/com/example/miapp/util/Constants.java
@@ -0,0 +1,7 @@
+package com.example.miapp.util;
+
+public class Constants {
+    public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
+    public static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+    public static final String LANGUAGE = "es-ES";
+}
